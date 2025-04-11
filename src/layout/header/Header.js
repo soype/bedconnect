6432c34e3481e9e209c5776dd774151c69cd1327
Pedro Henestroza <estudio@192.168.1.24>
2025-04-10 21:56:00 -0300

    Add cart to header, fix styles

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -2,22 +2,36 @@
 import Link from "next/link"
 import styles from './Header.module.scss'
 import { useAuth } from "@/context/AuthContext";
+import { useCart } from "@/context/CartContext";
+
+import { useState, useEffect } from "react";
 
 
 
 export default function Header() {
 
     const auth = useAuth();
+    const { cart } = useCart();
+
+    const [amount, setAmount] = useState(0);
+
+    useEffect(() => {
+        const totalAmount = cart.reduce((acc, item) => acc + item.amount, 0);
+        setAmount(totalAmount);
+    }, [cart]);
 
     return(
         <header className={styles.header}>
             <nav>
-                <h2>Bed Connect</h2>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    {auth.isLogged && <li><Link href="/catalogue">Catalogue</Link></li>}
-                    { auth.isLogged ? <li><Link href="/dashboard" className={styles.signin}>Dashboard</Link></li> : <li><Link href="/signin" className={styles.signin}>Sign in</Link></li>}
-                </ul>
+                <div className={styles.headerContainer}>
+                    <h2>Bed Connect</h2>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        {auth.isLogged && <li><Link href="/catalogue">Catalogue</Link></li>}
+                        {/* <li><Link href="/dashboard" className={styles.signin}>Dashboard</Link></li> */}
+                        { auth.isLogged ? <li>Cart: {amount}</li> : <li><Link href="/signin" className={styles.signin}>Sign in</Link></li>}
+                    </ul>
+                </div>
             </nav>
         </header>
     )
