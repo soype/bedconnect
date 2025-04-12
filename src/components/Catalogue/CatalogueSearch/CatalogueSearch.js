9e6d5a73e4bde7ab67bc12fc85fee063321e5e37
Pedro Henestroza <estudio@192.168.1.24>
2025-04-12 14:39:36 -0300

    Search bar styles

--- src/components/Catalogue/CatalogueSearch/CatalogueSearch.js
+++ src/components/Catalogue/CatalogueSearch/CatalogueSearch.js
@@ -1,34 +1,38 @@
-'use client'
+"use client";
 
-import {useState} from 'react';
+import { useState } from "react";
+import Image from "next/image";
+import searchIcon from '@/assets/search-svgrepo-com.svg';
+import styles from "./CatalogueSearch.module.scss";
 
-import styles from './CatalogueSearch.module.scss';
+export default function CatalogueSearch({ updateList }) {
+  const [parameter, setParameter] = useState("");
 
-export default function CatalogueSearch({updateList, hasSearched}) {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const parameter = e.target.elements.parameter.value;
+    console.log(parameter);
+    updateList(parameter);
+  };
 
-    const [parameter, setParameter] = useState('');
+  const parameterHandler = (e) => {
+    setParameter(e.target.value);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const parameter = e.target.elements.parameter.value;
-        console.log(parameter);
-        updateList(parameter);
-    }
+  const cleanHandler = () => {
+    updateList("");
+    setParameter("");
+  };
 
-    const parameterHandler = (e) => {
-        setParameter(e.target.value);
-    }
-
-    const cleanHandler = () => {
-        updateList('');
-        setParameter('');
-    }
-
-    return (
-        <form className={styles.catalogueSearch} onSubmit={submitHandler}>
-            <input type="text" name="parameter" value={parameter} onChange={parameterHandler} placeholder="Buscar por producto..."></input>
-            <button onClick={() => updateList('a')}>Buscar</button>
-            {hasSearched && <button onClick={cleanHandler}>Limpiar</button>}
-        </form>
-    )
+  return (
+    <form className={styles.catalogueSearch} onSubmit={submitHandler}>
+      <div className={styles.catalogueSearch__container}>
+        <input type="text" name="parameter" value={parameter} onChange={parameterHandler} placeholder="Buscar producto..."></input>
+        <button onClick={() => updateList("a")}>
+            <Image src={searchIcon} alt="search" width={20} height={20} />
+        </button>
+      </div>
+      {parameter.length > 0 && <button className={styles.clean} onClick={cleanHandler}>Limpiar búsqueda</button>}
+    </form>
+  );
 }
